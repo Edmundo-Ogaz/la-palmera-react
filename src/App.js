5693b73cb4db54d5a730996fe9e669a65a88402f
Edmundo Ogaz Baltierra <ext_eogazb@falabella.cl>
2023-01-07 14:17:16 -0300

    vercel

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Login from './pages/login/Login';
 
-import PrivateRoute from './utils/PrivateRoute';
+import PrivateRoute from './utils/PrivateRoute.js';
 import PublicRoute from './utils/PublicRoute';
 
 import { getToken, getEspiration } from './services/sessionStorage';
